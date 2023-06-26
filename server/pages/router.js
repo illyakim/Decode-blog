7693157897bb4bf820bac35ef717802a0afd0f5d
illyakim <ilyakm93@gmail.com>
2023-06-26 18:59:16 +0600

    profile error

--- server/pages/router.js
+++ server/pages/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Categories = require('../Categories/Categories')
+const User = require('../auth/User')
 
 router.get('/', async (req, res) => {
     const allCategories = await Categories.find()
@@ -15,8 +16,14 @@ router.get('/register', (req, res) => {
     res.render('register.ejs', { user: req.user ? req.user : {} })
 })
 
-router.get('/profile/:id', (req, res) => {
-    res.render('profile.ejs', { user: req.user ? req.user : {} })
+router.get('/profile/:id', async (req, res) => {
+    const allCategories = await Categories.find()
+    const user = await User.findById(req.params.id)
+    if (user) {
+        res.render("profile", { user: user, categories: allCategories, loginUser: req.user })
+    } else {
+        res.redirect('/not-found')
+    }
 })
 
 router.get('/newblog', async (req, res) => {
@@ -24,4 +31,11 @@ router.get('/newblog', async (req, res) => {
     res.render('new-blog', { categories: allCategories, user: req.user ? req.user : {} })
 })
 
+
+router.get('/not-found', (req, res) => {
+    res.render("notFound")
+})
+
 module.exports = router
+
+// Пишет loginUser is not defined
